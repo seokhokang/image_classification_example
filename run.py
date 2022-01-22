de4550fdaea21abd39c2c05f5d22536ddc0eca7e
Seokho Kang <39222137+seokhokang@users.noreply.github.com>
2022-01-22 11:34:56 +0900

    minor fix for svhn dataset

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ from sklearn.metrics import accuracy_score, log_loss
 
 
 dataset = sys.argv[1]
+use_trained = False
 model_path = './model_%s.pt'%dataset
 batch_size = 64
 
@@ -32,6 +33,8 @@ elif dataset == 'svhn':
     ])
     trnval_set = D.SVHN(root='./data/svhn', split='train', download=True, transform = transform)
     tst_set = D.SVHN(root='./data/svhn', split='test', download=True, transform = transform)
+    trnval_set.targets = trnval_set.labels
+    tst_set.targets = tst_set.labels
 
 elif dataset == 'fashionmnist':      
     transform = T.Compose([
@@ -41,7 +44,7 @@ elif dataset == 'fashionmnist':
     ])                
     trnval_set = D.FashionMNIST(root='./data/fashionmnist', train=True, download=True, transform = transform)
     tst_set = D.FashionMNIST(root='./data/fashionmnist', train=False, download=True, transform = transform)
-        
+
 elif dataset == 'mnist':
     transform = T.Compose([
         T.ToTensor(),
@@ -51,6 +54,7 @@ elif dataset == 'mnist':
     trnval_set = D.MNIST(root='./data/mnist', train=True, download=True, transform = transform)
     tst_set = D.MNIST(root='./data/mnist', train=False, download=True, transform = transform)
 
+classes = np.unique(trnval_set.targets)
 val_size = int(0.2 * len(trnval_set))
 trn_size = len(trnval_set) - val_size
 trn_set, val_set = random_split(trnval_set, [trn_size, val_size])
@@ -64,8 +68,11 @@ tst_loader = DataLoader(dataset=tst_set, batch_size=batch_size, shuffle=False)
 
 ## training
 cuda = torch.device('cuda:0')
-net = CNN(len(trnval_set.classes)).to(cuda)
-net = training(net, trn_loader, val_loader, model_path, cuda)
+net = CNN(len(classes)).to(cuda)
+if use_trained == True:
+    net.load_state_dict(torch.load(model_path))
+else:
+    net = training(net, trn_loader, val_loader, model_path, cuda)
 
 ## test
 tst_y = np.array(tst_set.targets)
