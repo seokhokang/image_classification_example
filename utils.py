ccb6515e01fc77f01ff694df4f0a463c81dc5c22
dmson <jwson0308@gmail.com>
2022-01-26 18:21:52 +0900

    add augmentation

--- utils.py
+++ utils.py
@@ -4,12 +4,16 @@ from torch import nn
 from torchvision import transforms as T
 
 
-def get_augmentation(degree = 30, hshift = 0.3, vshift = 0.3, scale = 0.2, shear = 15, prob_hflip = 0, prob_vflip = 0):
+def get_augmentation(prob_equal = 0, prob_invert = 0, brightness = 1, contrast = 1, saturation = 1, degree = 30, hshift = 0.3, vshift = 0.3, scale = 0.2, shearx = 0, sheary = 0, prob_hflip = 0, prob_vflip = 0):
     
     augmentation_operator = T.Compose([
-        T.RandomAffine(degrees = degree, translate = (hshift, vshift), scale = (1-scale, 1+scale), shear = shear),
+        T.RandomEqualize(prob_equal),
+        T.RandomInvert(prob_invert),
+        T.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation),
+        T.RandomAffine(degrees = degree, translate = (hshift, vshift), scale = (1-scale, 1+scale), shear = (-shearx, shearx, -sheary, sheary)),
         T.RandomHorizontalFlip(prob_hflip),
         T.RandomVerticalFlip(prob_vflip),
+        T.ToTensor(),
         T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     
@@ -19,6 +23,7 @@ def get_augmentation(degree = 30, hshift = 0.3, vshift = 0.3, scale = 0.2, shear
 def get_no_augmentation():
 
     augmentation_operator = T.Compose([
+        T.ToTensor(),
         T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     
